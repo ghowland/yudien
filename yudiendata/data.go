5ca5769b8a6f841d12583601e6a45a1a2feb3274
zelin-l <zelinliu123@gmail.com>
2018-03-14 16:47:43 -0700

    Added flag to toggle statements printed to console/file

--- yudiendata/data.go
+++ yudiendata/data.go
@@ -66,7 +66,7 @@ func GetSelectedDb(db_web *sql.DB, db *sql.DB, db_id int64) *sql.DB {
 }
 
 func Query(db *sql.DB, sql string) []map[string]interface{} {
-	fmt.Printf("Query: %s\n", sql)
+	Printf("Query: %s\n", sql)
 
 	// Query
 	rs, err := db.Query(sql)
@@ -96,7 +96,7 @@ func Query(db *sql.DB, sql string) []map[string]interface{} {
 		template_map := make(map[string]interface{})
 
 		for key, value := range fb.GetFieldArr() {
-			//fmt.Printf("Found value: %s = %s\n", key, value)
+			//Printf("Found value: %s = %s\n", key, value)
 
 			switch value.(type) {
 			case []byte:
@@ -117,7 +117,7 @@ func Query(db *sql.DB, sql string) []map[string]interface{} {
 }
 
 func DatamanGet(collection_name string, record_id int, options map[string]interface{}) map[string]interface{} {
-	//fmt.Printf("DatamanGet: %s: %d\n", collection_name, record_id)
+	//Printf("DatamanGet: %s: %d\n", collection_name, record_id)
 
 	get_map := map[string]interface{}{
 		"db":             DatabaseTarget,
@@ -128,15 +128,15 @@ func DatamanGet(collection_name string, record_id int, options map[string]interf
 		"join": options["join"],
 	}
 
-	//fmt.Printf("Dataman Get: %v\n\n", get_map)
+	//Printf("Dataman Get: %v\n\n", get_map)
 
 	dataman_query := &query.Query{query.Get, get_map}
 
 	result := DatasourceInstance["opsdb"].HandleQuery(context.Background(), dataman_query)
 
-	//fmt.Printf("Dataman GET: %v\n", result.Return[0])
+	//Printf("Dataman GET: %v\n", result.Return[0])
 	if result.Error != "" {
-		fmt.Printf("Dataman GET: ERRORS: %v\n", result.Error)
+		Printf("Dataman GET: ERRORS: %v\n", result.Error)
 	}
 
 	return result.Return[0]
@@ -149,10 +149,10 @@ func DatamanSet(collection_name string, record map[string]interface{}) map[strin
 
 	// Remove the _id field, if it is nil.  This means it should be new/insert
 	if record["_id"] == nil || record["_id"] == "<nil>" || record["_id"] == "\u003cnil\u003e" || record["_id"] == "" {
-		//fmt.Printf("DatamanSet: Removing _id key: %s\n", record["_id"])
+		//Printf("DatamanSet: Removing _id key: %s\n", record["_id"])
 		delete(record, "_id")
 	} else {
-		//fmt.Printf("DatamanSet: Not Removing _id: %s\n", record["_id"])
+		//Printf("DatamanSet: Not Removing _id: %s\n", record["_id"])
 	}
 
 	// Fix data manually, for now
@@ -168,7 +168,7 @@ func DatamanSet(collection_name string, record map[string]interface{}) map[strin
 	// ng any values
 	//TODO(g):REMOVE: This is fixing up implementation problems in Dataman, which Thomas will fix...
 	if record["_id"] != nil && record["_id"] != "" {
-		//fmt.Printf("Ensuring all fields are present (HACK): %s: %v\n", collection_name, record["_id"])
+		//Printf("Ensuring all fields are present (HACK): %s: %v\n", collection_name, record["_id"])
 
 		// Record ID will be an integer
 		var record_id int64
@@ -192,14 +192,14 @@ func DatamanSet(collection_name string, record map[string]interface{}) map[strin
 		//for k, v := range record_current {
 		//	if record[k] == nil {
 		//		record[k] = v
-		//		fmt.Printf("Adding field: %s: %s: %v\n", collection_name, k, v)
+		//		Printf("Adding field: %s: %s: %v\n", collection_name, k, v)
 		//	}
 		//}
 
 		// Remove any fields that arent present in the record_current
 		for k, _ := range record {
 			if _, has_key := record_current[k]; !has_key {
-				fmt.Printf("Removing field: %s: %s: %v\n", collection_name, k, record[k])
+				Printf("Removing field: %s: %s: %v\n", collection_name, k, record[k])
 				delete(record, k)
 			}
 		}
@@ -224,17 +224,17 @@ func DatamanSet(collection_name string, record map[string]interface{}) map[strin
 		},
 	}
 
-	//fmt.Printf("Dataman SET: Record: %v\n", record)
-	//fmt.Printf("Dataman SET: Record: JSON: %v\n", JsonDump(record))
-	//fmt.Printf("Dataman SET: Query: JSON: %v\n", JsonDump(dataman_query))
+	//Printf("Dataman SET: Record: %v\n", record)
+	//Printf("Dataman SET: Record: JSON: %v\n", JsonDump(record))
+	//Printf("Dataman SET: Query: JSON: %v\n", JsonDump(dataman_query))
 
 	result := DatasourceInstance["opsdb"].HandleQuery(context.Background(), dataman_query)
 
 	//result_bytes, _ := json.Marshal(result)
-	//fmt.Printf("Dataman SET: %s\n", result_bytes)
+	//Printf("Dataman SET: %s\n", result_bytes)
 
 	if result.Error != "" {
-		fmt.Printf("Dataman SET: ERROR: %v\n", result.Error)
+		Printf("Dataman SET: ERROR: %v\n", result.Error)
 	}
 
 	if result.Return != nil {
@@ -246,8 +246,8 @@ func DatamanSet(collection_name string, record map[string]interface{}) map[strin
 
 func DatamanFilter(collection_name string, filter map[string]interface{}, options map[string]interface{}) []map[string]interface{} {
 
-	//fmt.Printf("DatamanFilter: %s:  Filter: %v  Join: %v\n\n", collection_name, filter, options["join"])
-	//fmt.Printf("Sort: %v\n", options["sort"])		//TODO(g): Sorting
+	//Printf("DatamanFilter: %s:  Filter: %v  Join: %v\n\n", collection_name, filter, options["join"])
+	//Printf("Sort: %v\n", options["sort"])		//TODO(g): Sorting
 
 	filter = MapCopy(filter)
 
@@ -271,18 +271,18 @@ func DatamanFilter(collection_name string, filter map[string]interface{}, option
 		//"sort_reverse":	  []bool{true},
 	}
 
-	//fmt.Printf("Dataman Filter: %s\n\n", JsonDump(filter_map))
-	//fmt.Printf("Dataman Filter Map Filter: %s\n\n", SnippetData(filter_map["filter"], 120))
-	//fmt.Printf("Dataman Filter Map Filter Array: %s\n\n", SnippetData(filter_map["filter"].(map[string]interface{})["name"], 120))
+	//Printf("Dataman Filter: %s\n\n", JsonDump(filter_map))
+	//Printf("Dataman Filter Map Filter: %s\n\n", SnippetData(filter_map["filter"], 120))
+	//Printf("Dataman Filter Map Filter Array: %s\n\n", SnippetData(filter_map["filter"].(map[string]interface{})["name"], 120))
 
 	dataman_query := &query.Query{query.Filter, filter_map}
 
 	result := DatasourceInstance["opsdb"].HandleQuery(context.Background(), dataman_query)
 
 	if result.Error != "" {
-		fmt.Printf("Dataman ERROR: %v\n", result.Error)
+		Printf("Dataman ERROR: %v\n", result.Error)
 	} else {
-		//fmt.Printf("Dataman FILTER: %v\n", result.Return)
+		Printf("Dataman FILTER: %v\n", result.Return)
 	}
 
 	return result.Return
@@ -298,8 +298,8 @@ func DatamanFilterFull(collection_name string, filter interface{}, options map[s
 	// ex: multi filter:
 	//     [{field1=value1}, "AND", {field2=value2}] (type: []interface{})
 	//     [{field1=value1}, "AND", [{field2=value2}, "AND", {field3=value3}]]
-	fmt.Printf("DatamanFilter: %s:  Filter: %v  Join: %v\n\n", collection_name, filter, options["join"])
-	//fmt.Printf("Sort: %v\n", options["sort"])		//TODO(g): Sorting
+	Printf("DatamanFilter: %s:  Filter: %v  Join: %v\n\n", collection_name, filter, options["join"])
+	//Printf("Sort: %v\n", options["sort"])		//TODO(g): Sorting
 
 	filter_map := map[string]interface{}{
 		"db":             "opsdb",
@@ -311,8 +311,8 @@ func DatamanFilterFull(collection_name string, filter interface{}, options map[s
 		//"sort_reverse":	  []bool{true},
 	}
 
-	fmt.Printf("Dataman Filter: %v\n\n", filter_map)
-	fmt.Printf("Dataman Filter Map Filter: %s\n\n", SnippetData(filter_map["filter"], 120))
+	Printf("Dataman Filter: %v\n\n", filter_map)
+	Printf("Dataman Filter Map Filter: %s\n\n", SnippetData(filter_map["filter"], 120))
 
 
 	dataman_query := &query.Query{query.Filter, filter_map}
@@ -320,9 +320,9 @@ func DatamanFilterFull(collection_name string, filter interface{}, options map[s
 	result := DatasourceInstance["opsdb"].HandleQuery(context.Background(), dataman_query)
 
 	if result.Error != "" {
-		fmt.Printf("Dataman ERROR: %v\n", result.Error)
+		Printf("Dataman ERROR: %v\n", result.Error)
 	} else {
-		fmt.Printf("Dataman FILTER: %v\n", result.Return)
+		Printf("Dataman FILTER: %v\n", result.Return)
 	}
 
 	return result.Return
@@ -358,7 +358,7 @@ func DatamanEnsureDatabases(pgconnect string, database string, current_path stri
 	schema_result := DatamanFilter("schema", filter_map, option_map)
 	schema_map := schema_result[0]
 
-	fmt.Printf("OpsDB Schema: %v\n\n", schema_map)
+	Printf("OpsDB Schema: %v\n\n", schema_map)
 
 	opsdb_schema := DatasourceInstance["opsdb"].StoreSchema
 
@@ -371,12 +371,12 @@ func DatamanEnsureDatabases(pgconnect string, database string, current_path stri
 
 
 
-	fmt.Printf("\nList DB Start: %v\n\n", time.Now().String())
+	Printf("\nList DB Start: %v\n\n", time.Now().String())
 	//db_list := opsdb_schema.ListDatabase(context.Background())
 	db_item := opsdb_schema.GetDatabase(context.Background(), limited_database_search)
-	fmt.Printf("\nList DB Stop: %v\n\n", time.Now().String())
+	Printf("\nList DB Stop: %v\n\n", time.Now().String())
 
-	fmt.Printf("\n\nFound DB Item: %v\n\n", db_item.Name)
+	Printf("\n\nFound DB Item: %v\n\n", db_item.Name)
 
 	//shard_instance := opsdb_schema.ListShardInstance(context.Background(), db_item.Name)
 
@@ -385,12 +385,12 @@ func DatamanEnsureDatabases(pgconnect string, database string, current_path stri
 	collection_array := make([]string, 0)
 
 
-	//fmt.Printf("\n\nFound DB Shard Instance: %v -- %v\n\n", shard_instance, db_item.ShardInstances["public"])
+	//Printf("\n\nFound DB Shard Instance: %v -- %v\n\n", shard_instance, db_item.ShardInstances["public"])
 
 	//collections := opsdb_schema.ListCollection(context.Background(), db_item.Name, "public")
 
 	for _, collection := range db_item.ShardInstances["public"].Collections {
-		fmt.Printf("\n\nFound DB Collections: %s\n", collection.Name)
+		Printf("\n\nFound DB Collections: %s\n", collection.Name)
 
 		option_map := make(map[string]interface{})
 		filter_map := make(map[string]interface{})
@@ -401,7 +401,7 @@ func DatamanEnsureDatabases(pgconnect string, database string, current_path stri
 		collection_map := make(map[string]interface{})
 		if len(collection_result) > 0 {
 			collection_map = collection_result[0]
-			fmt.Printf("OpsDB Collection: %v\n\n", collection_map)
+			Printf("OpsDB Collection: %v\n\n", collection_map)
 
 			// Add collection to our tracking array
 			collection_array = append(collection_array, collection.Name)
@@ -413,7 +413,7 @@ func DatamanEnsureDatabases(pgconnect string, database string, current_path stri
 			// Check: Index, Primary Index
 
 			for _, field := range collection.Fields {
-				fmt.Printf("\n\nFound DB Collections: %s  Field: %s  Type: %s   (Default: %v -- Not Null: %v -- Relation: %v)\n", collection.Name, field.Name, field.FieldType.Name, field.Default, field.NotNull, field.Relation)
+				Printf("\n\nFound DB Collections: %s  Field: %s  Type: %s   (Default: %v -- Not Null: %v -- Relation: %v)\n", collection.Name, field.Name, field.FieldType.Name, field.Default, field.NotNull, field.Relation)
 
 				// Check: Not Null, Relation, Default, Type
 
@@ -426,19 +426,19 @@ func DatamanEnsureDatabases(pgconnect string, database string, current_path stri
 				collection_field_map := make(map[string]interface{})
 				if len(collection_field_result) > 0 {
 					collection_field_map = collection_field_result[0]
-					fmt.Printf("OpsDB Collection Field: %v\n\n", collection_field_map)
+					Printf("OpsDB Collection Field: %v\n\n", collection_field_map)
 
 					// Add collection to our tracking array
 					collection_field_array = append(collection_field_array, field.Name)
 
 
 				} else {
-					fmt.Printf("OpsDB Collection Field: MISSING: %s\n\n", field.Name)
+					Printf("OpsDB Collection Field: MISSING: %s\n\n", field.Name)
 
 				}
 			}
 		} else {
-			fmt.Printf("OpsDB Collection: MISSING: %s\n\n", collection.Name)
+			Printf("OpsDB Collection: MISSING: %s\n\n", collection.Name)
 
 		}
 	}
@@ -457,7 +457,7 @@ func DatamanEnsureDatabases(pgconnect string, database string, current_path stri
 
 	for _, collection_record := range all_collection_result {
 		if ok, _ := InArray(collection_record["name"].(string), collection_array) ; !ok {
-			fmt.Printf("Not Found collection: %s\n\n", collection_record["name"])
+			Printf("Not Found collection: %s\n\n", collection_record["name"])
 
 
 			new_collection := metadata.Collection{}
@@ -474,7 +474,7 @@ func DatamanEnsureDatabases(pgconnect string, database string, current_path stri
 
 			// Create the new collection
 			err := opsdb_schema.AddCollection(context.Background(), db_item, db_item.ShardInstances["public"], &new_collection)
-			fmt.Printf("Add New Collection: %s: ERROR: %s\n\n", new_collection.Name, err)
+			Printf("Add New Collection: %s: ERROR: %s\n\n", new_collection.Name, err)
 
 			for _, field_map := range all_collection_field_result {
 				// Create fields we need to populate this table
@@ -492,7 +492,7 @@ func DatamanEnsureDatabases(pgconnect string, database string, current_path stri
 
 				// Create the new collection field
 				err := opsdb_schema.AddCollectionField(context.Background(), db_item, db_item.ShardInstances["public"], &new_collection, &new_field)
-				fmt.Printf("Add New Collection Field: %s: %s: ERROR: %s\n\n", new_collection.Name, new_field.Name, err)
+				Printf("Add New Collection Field: %s: %s: ERROR: %s\n\n", new_collection.Name, new_field.Name, err)
 
 				if field_map["is_primary_key"] == true {
 					new_index := metadata.CollectionIndex{}
@@ -512,7 +512,7 @@ func DatamanEnsureDatabases(pgconnect string, database string, current_path stri
 					//
 					sql := fmt.Sprintf("ALTER TABLE %s ADD PRIMARY KEY (%s)", new_collection.Name, new_field.Name)
 					Query(db, sql)
-					fmt.Printf("Add New Collection Field PKEY: %s: %s: ERROR: %s\n\n", new_collection.Name, new_index.Name, err)
+					Printf("Add New Collection Field PKEY: %s: %s: ERROR: %s\n\n", new_collection.Name, new_index.Name, err)
 
 				}
 
@@ -564,7 +564,7 @@ func DatamanEnsureDatabases(pgconnect string, database string, current_path stri
 	/*
 	for _, db_item := range db_list {
 		if db_item.Name == limited_database_search {
-			fmt.Printf("\n\nFound DB Item: %v\n\n", db_item.Name)
+			Printf("\n\nFound DB Item: %v\n\n", db_item.Name)
 
 		}
 
@@ -588,7 +588,7 @@ func DatamanEnsureDatabases(pgconnect string, database string, current_path stri
 
 
 	/*
-	fmt.Printf("Starting ensure DB processs...\n")
+	Printf("Starting ensure DB processs...\n")
 
 	config := storagenode.DatasourceInstanceConfig{
 		StorageNodeType: "postgres",
@@ -609,7 +609,7 @@ func DatamanEnsureDatabases(pgconnect string, database string, current_path stri
 		}
 	}
 
-	//fmt.Printf("Schema STR: %s\n\n", schema_str)
+	//Printf("Schema STR: %s\n\n", schema_str)
 
 	var meta metadata.Meta
 	err = json.Unmarshal(schema_str, &meta)
@@ -617,7 +617,7 @@ func DatamanEnsureDatabases(pgconnect string, database string, current_path stri
 		panic("Cannot parse JSON config data: " + err.Error())
 	}
 
-	fmt.Printf("Creating new data source instances...\n")
+	Printf("Creating new data source instances...\n")
 
 
 	//if datasource, err := storagenode.NewLocalDatasourceInstance(&config, &meta); err == nil {
@@ -632,7 +632,7 @@ func DatamanEnsureDatabases(pgconnect string, database string, current_path stri
 	//DatasourceInstance["opsdb"] = datasource
 	//DatasourceConfig["opsdb"] = &config
 
-	fmt.Printf("Importing current database info...\n")
+	Printf("Importing current database info...\n")
 
 
 	// Load meta
@@ -646,18 +646,18 @@ func DatamanEnsureDatabases(pgconnect string, database string, current_path stri
 		panic(fmt.Sprintf("Error loading meta: %v", err))
 	}
 
-	fmt.Printf("Creating the current DB as ensure...\n")
+	Printf("Creating the current DB as ensure...\n")
 
 	for _, db := range meta.Databases {
 		//name := db.Name	//TODO(g): Make the names dynamic, with the DBs too, need to make it all line up
-		fmt.Printf("Ensuring DB: %s\n", db.Name)
+		Printf("Ensuring DB: %s\n", db.Name)
 		err := DatasourceInstance["opsdb"].MutableMetaStore.EnsureExistsDatabase(context.Background(), db)
 		if err != nil {
 			panic(err)
 		}
 	}
 
-	fmt.Printf("Creating the NEW DB as ensure...\n")
+	Printf("Creating the NEW DB as ensure...\n")
 
 	// Load meta
 	meta2 = &metadata.Meta{}
@@ -672,14 +672,14 @@ func DatamanEnsureDatabases(pgconnect string, database string, current_path stri
 
 	for _, db := range meta.Databases {
 		//name := db.Name	//TODO(g): Make the names dynamic, with the DBs too, need to make it all line up
-		fmt.Printf("Ensuring DB: %s\n", db.Name)
+		Printf("Ensuring DB: %s\n", db.Name)
 		err := DatasourceInstance["opsdb"].EnsureExistsDatabase(context.Background(), db)
 		if err != nil {
 			panic(err)
 		}
 	}
 
-	fmt.Printf("Finished Creating the NEW DB as ensure...\n")
+	Printf("Finished Creating the NEW DB as ensure...\n")
 	*/
 
 
@@ -737,7 +737,7 @@ func InitDataman(pgconnect string, database string) {
 		}
 	}
 
-	//fmt.Printf("Schema STR: %s\n\n", schema_str)
+	//Printf("Schema STR: %s\n\n", schema_str)
 
 	var meta metadata.Meta
 	err = json.Unmarshal(schema_str, &meta)
