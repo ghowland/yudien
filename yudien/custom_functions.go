911461ad653913fb2a1ada8d5581c4aa8f0c09c5
Geoff Howland <geoff@gmail.com>
2018-06-09 17:41:03 -0700

    Schedule population data is ready, next is doing time maths

--- yudien/custom_functions.go
+++ yudien/custom_functions.go
@@ -5,31 +5,91 @@ import (
 	. "github.com/ghowland/yudien/yudiencore"
 	. "github.com/ghowland/yudien/yudiendata"
 	. "github.com/ghowland/yudien/yudienutil"
+	"time"
+	"strings"
 )
 
 func UDN_Customer_PopulateScheduleDutyResponsibility(db *sql.DB, udn_schema map[string]interface{}, udn_start *UdnPart, args []interface{}, input interface{}, udn_data map[string]interface{}) UdnResult {
 	database := GetResult(args[0], type_string).(string)
 	responsibility_id := GetResult(args[1], type_int).(int64)
-	//start_populating := GetResult(args[2], type_string).(string)
+	start_populating := GetResult(args[2], type_string).(string)
+	//business_user_id := GetResult(args[3], type_string).(int64)
 
-	UdnLogLevel(udn_schema, log_debug, "CUSTOM: Populate Schedule: Duty Responsibility: %v", input)
+	start_populating = strings.Replace(start_populating," ", "T", -1)
 
-	options := make(map[string]interface{})
-	options["db"] = database
-	responsibility := DatamanGet("duty_responsibility", int(responsibility_id), options)
+	start_time, err := time.Parse("2006-01-02T15:04:05", start_populating)
+
+	UdnLogLevel(udn_schema, log_debug, "CUSTOM: Populate Schedule: Duty Responsibility: %v\n", start_time, err)
+
 
 	result := UdnResult{}
 	result.Result = nil
 
-	// If we didnt get an error
+	options := make(map[string]interface{})
+	options["db"] = database
+
+	// Get the Responsibility
+	responsibility := DatamanGet("duty_responsibility", int(responsibility_id), options)
 	if responsibility["_error"] != nil {
-		UdnLogLevel(udn_schema, log_debug, "CUSTOM: Populate Schedule: Duty Responsibility: Error getting responsibility: %v", responsibility["_error"])
+		UdnLogLevel(udn_schema, log_debug, "CUSTOM: Populate Schedule: Duty Responsibility: Error getting responsibility: %v\n", responsibility["_error"])
 		return result
 	}
 
-	UdnLogLevel(udn_schema, log_debug, "CUSTOM: Populate Schedule: Duty Responsibility: Responsibility: %v", responsibility)
+	UdnLogLevel(udn_schema, log_debug, "CUSTOM: Populate Schedule: Duty Responsibility: Responsibility: %v\n", responsibility)
 
-	UdnLogLevel(udn_schema, log_debug, "CUSTOM: Populate Schedule: Duty Responsibility: Result: %v", result.Result)
+	// Get the Roster
+	roster := DatamanGet("duty_roster", int(responsibility["duty_roster_id"].(int64)), options)
+	if roster["_error"] != nil {
+		UdnLogLevel(udn_schema, log_debug, "CUSTOM: Populate Schedule: Duty Responsibility: Error getting Roster: %v\n", roster["_error"])
+		return result
+	}
+	UdnLogLevel(udn_schema, log_debug, "CUSTOM: Populate Schedule: Duty Responsibility: Roster: %v\n", roster)
+
+	// Get the Roster Users ordered by priority
+	options["sort"] = []string{"priority"}
+	filter := map[string]interface{}{
+		"duty_roster_id": []interface{}{2, "=", roster["_id"]},
+	}
+	roster_users := DatamanFilter("duty_roster_business_user", filter, options)
+	if len(roster_users) == 0 {
+		UdnLogLevel(udn_schema, log_debug, "CUSTOM: Populate Schedule: Duty Responsibility: Error getting Roster Users: %d\n", len(roster_users))
+		return result
+	}
+	UdnLogLevel(udn_schema, log_debug, "CUSTOM: Populate Schedule: Duty Responsibility: Roster Users: %v\n", roster_users)
+
+	// Get the Duty Responsbility Shifts
+	options["sort"] = nil
+	filter = map[string]interface{}{
+		"duty_responsibility_id": []interface{}{2, "=", responsibility["_id"]},
+	}
+	shifts := DatamanFilter("duty_responsibility_shift", filter, options)
+	if responsibility["_error"] != nil {
+		UdnLogLevel(udn_schema, log_debug, "CUSTOM: Populate Schedule: Duty Responsibility: Error getting Shifts: %d\n", len(shifts))
+		return result
+	}
+	UdnLogLevel(udn_schema, log_debug, "CUSTOM: Populate Schedule: Duty Responsibility: Shifts: %v\n", shifts)
+
+	// Get the Timeline
+	timeline := DatamanGet("schedule_timeline", int(responsibility["schedule_timeline_id"].(int64)), options)
+	if timeline["_error"] != nil {
+		UdnLogLevel(udn_schema, log_debug, "CUSTOM: Populate Schedule: Duty Responsibility: Error getting Schedule Timeline: %v\n", timeline["_error"])
+		return result
+	}
+	UdnLogLevel(udn_schema, log_debug, "CUSTOM: Populate Schedule: Duty Responsibility: Schedule Timeline: %v\n", timeline)
+
+	// Get the Schedule Timeline Items
+	options["sort"] = []string{"time_start"}
+	filter = map[string]interface{}{
+		"schedule_timeline_id": []interface{}{2, "=", responsibility["schedule_timeline_id"]},
+	}
+	timeline_items := DatamanFilter("schedule_timeline_item", filter, options)
+	if len(timeline_items) == 0 {
+		UdnLogLevel(udn_schema, log_debug, "CUSTOM: Populate Schedule: Duty Responsibility: Error getting Schedule Timeline Items: %d\n", len(timeline_items))
+		return result
+	}
+	UdnLogLevel(udn_schema, log_debug, "CUSTOM: Populate Schedule: Duty Responsibility: Schedule Timeline Items: %v\n", timeline_items)
+
+	UdnLogLevel(udn_schema, log_debug, "CUSTOM: Populate Schedule: Duty Responsibility: Result: %v\n", result.Result)
 
 	return result
 }
